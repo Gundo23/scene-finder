be20084410118713d4fa961d6a5fffd1b86234f4
Gundo23 <raymc2004@hotmail.com>
2026-06-07 16:31:45 +0100

    Fix HTML currency entity decoding

--- src/lib/cleanText.ts
+++ src/lib/cleanText.ts
@@ -4,16 +4,32 @@ export function cleanText(text: string | null | undefined) {
   return text
     .replace(/&#038;/g, '&')
     .replace(/&amp;/g, '&')
+
+    .replace(/&pound;/gi, '£')
+    .replace(/&#163;/g, '£')
+
+    .replace(/&euro;/gi, '€')
+    .replace(/&#8364;/g, '€')
+
+    .replace(/&dollar;/gi, '$')
+    .replace(/&#36;/g, '$')
+
     .replace(/&gt;/g, '>')
     .replace(/&lt;/g, '<')
+
     .replace(/&nbsp;/g, ' ')
+    .replace(/&#160;/g, ' ')
+
     .replace(/&#8217;/g, "'")
     .replace(/&#8211;/g, '-')
     .replace(/&#8212;/g, '—')
+
     .replace(/&#8220;/g, '"')
     .replace(/&#8221;/g, '"')
+
     .replace(/&#39;/g, "'")
     .replace(/&quot;/g, '"')
+
     .replace(/\s+/g, ' ')
     .trim()
 }
